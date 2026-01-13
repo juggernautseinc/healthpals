661d846341391b979e07dd82c2481b22a16fbb0a
juggernautsei <sherwingaddis@gmail.com>
2026-01-13 04:13:10 -0500

    Decrypt results files

--- decrypt_hl7.php
+++ decrypt_hl7.php
@@ -13,8 +13,8 @@ require_once(__DIR__ . '/interface/globals.php');
 
 use OpenEMR\Common\Crypto\CryptoGen;
 
-$directory = '/var/www/vhosts/healthpals.co/emr.healthpals.co/openemr-7.0.3/sites/default/documents/procedure_results/1-98765432';
-$encrypted_contents = __DIR__ . 'sites/default/documents/procedure_results/1-98765432/quest_results_2026-01-08 18:20:35_1.hl7';
+$directory = __DIR__ . '/sites/default/documents/procedure_results/1-98765432';
+$encrypted_contents = __DIR__ . '/sites/default/documents/procedure_results/1-98765432/quest_results_2026-01-08 18:20:35_1.hl7';
 
 $whatsinthere = file_get_contents($encrypted_contents);
 
